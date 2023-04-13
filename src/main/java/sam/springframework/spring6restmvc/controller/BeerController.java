f8a005b960429094d2d2083c27d5ff66128ae271
Samuel <samuel.b.rodrigues@accenture.com>
2023-04-13 11:39:41 -0300

    http get by id

--- src/main/java/sam/springframework/spring6restmvc/controller/BeerController.java
+++ src/main/java/sam/springframework/spring6restmvc/controller/BeerController.java
@@ -2,8 +2,9 @@ package sam.springframework.spring6restmvc.controller;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import sam.springframework.spring6restmvc.model.Beer;
 import sam.springframework.spring6restmvc.services.BeerService;
@@ -14,18 +15,20 @@ import java.util.UUID;
 @Slf4j
 @AllArgsConstructor
 @RestController
+@RequestMapping("/api/v1/beer")
 public class BeerController {
     private final BeerService beerService;
 
-    @RequestMapping("/api/v1/beer")
+    @RequestMapping(method = RequestMethod.GET)
     public List<Beer> listBeer(){
         return beerService.listBeers();
     }
 
-    public Beer getBeerById(UUID id) {
+    @RequestMapping(value = "{beerId}", method = RequestMethod.GET)
+    public Beer getBeerById(@PathVariable("beerId") UUID beerId) {
 
         log.debug("Get Beer by Id - in controller");
 
-        return beerService.getBeerById(id);
+        return beerService.getBeerById(beerId);
     }
 }
