4d9e3f4ff8bb69246bcc628cb6fd682a2d58e689
Samuel <samuel.b.rodrigues@accenture.com>
2023-04-13 15:19:31 -0300

    mock mvc test

--- src/main/java/sam/springframework/spring6restmvc/controller/CustomerController.java
+++ src/main/java/sam/springframework/spring6restmvc/controller/CustomerController.java
@@ -1,6 +1,6 @@
 package sam.springframework.spring6restmvc.controller;
 
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.UUID;
 
 @Slf4j
-@AllArgsConstructor
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/customer")
 public class CustomerController {
