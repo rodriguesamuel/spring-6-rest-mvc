79fd75aa006ce213512c76c72d7e98848fac2e33
Samuel <samuel.b.rodrigues@accenture.com>
2023-04-14 14:46:42 -0300

    creating entities JPA

---
+++ src/main/java/sam/springframework/spring6restmvc/entities/Customer.java
@@ -0,0 +1,27 @@
+package sam.springframework.spring6restmvc.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Version;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Customer {
+
+    @Id
+    private UUID id;
+
+    @Version
+    private Integer version;
+    private String customerName;
+    private LocalDateTime createdDate;
+    private LocalDateTime lastModifiedDate;
+}
